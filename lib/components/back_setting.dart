1cf5cd1678e1f18bdf36ab6d3d2ed3f49ab86453
Nuthara <nutharayuthmini@gmail.com>
2023-07-27 13:43:10 +0530

    Adding some component widgets

---
+++ lib/components/back_setting.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class BackSetting extends StatefulWidget {
+  const BackSetting({super.key});
+
+  @override
+  State<BackSetting> createState() => _BackSettingState();
+}
+
+class _BackSettingState extends State<BackSetting> {
+   @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            top: 39,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 60,
+              color: Colors.white, // Customize your background color here
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //Menu Icon
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/icons/back.png',
+                      color: Colors.black,
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {},
+                  ),
+                  //Centered text
+                      Text(
+                        'My Profile',
+                        style: TextStyle(color: Colors.black, fontSize: 17),
+                      ),
+                  //Notification icon
+                  IconButton(
+                    icon: Image.asset(
+                      'assets/icons/setting.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 80,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Center(
+              child: Text(
+                'Your main content goes here',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

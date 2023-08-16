a18c053bb27b7d8a3a5a668c8ebb128e487f3dae
nawam727 <nawamdenawakage@gmail.com>
2023-08-16 14:41:09 +0530

    Updated fuction

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
 class MyButton extends StatelessWidget {
-
   final Function()? onTap;
 
   const MyButton({super.key, required this.onTap});
@@ -18,17 +17,17 @@ class MyButton extends StatelessWidget {
           color: HexColor("#00B251"),
           borderRadius: BorderRadius.circular(10),
           border: Border.all(
-                color: Colors.black, // Set your desired border color here
-                width: 0.3,
+            color: Colors.black, // Set your desired border color here
+            width: 0.3,
           ),
         ),
         child: Center(
-          child: Text("Login",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-            color: HexColor("#ffffff")
-            ),
+          child: Text(
+            "Login",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+                color: HexColor("#ffffff")),
           ),
         ),
       ),

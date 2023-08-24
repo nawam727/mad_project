f0c517a9064442eae57e4c5ad4403cc8aed0f9b4
Nuthara <nutharayuthmini@gmail.com>
2023-08-24 19:58:18 +0530

    Adding settings page

---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:mad_project/components/back_dots.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:mad_project/widgets/settings_tile.dart';
+
+
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BackDots(
+        title: "Settings",
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              //account
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.person_outline,
+                title: "Account",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //notification
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.notifications_outline,
+                title: "Notification",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //location
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.location_outline,
+                title: "Location",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //terms and conditions
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.document_outline,
+                title: "Terms and Conditions",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              //software update
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.arrow_up_outline,
+                title: "Software Update",
+                onTap: () {},
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              //logout
+              SettingsTile(
+                color: const Color.fromARGB(255, 255, 255, 255),
+                icon: Ionicons.log_out_outline,
+                title: "Logout",
+                onTap: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+

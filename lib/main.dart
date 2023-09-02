67210a85157353298a86cc843ed79869f2fe91bd
KavindiViranga <97431820+KavindiViranga@users.noreply.github.com>
2023-09-02 16:00:23 +0530

    Create feedback page

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mad_project/components/nav_bar.dart';
 import 'package:mad_project/firebase_options.dart';
+import 'package:mad_project/pages/feedback_page.dart';
 import 'package:mad_project/pages/hall_availability.dart';
 import 'package:mad_project/pages/login_page.dart';
 import 'package:mad_project/pages/map_navigation_page.dart';
@@ -87,7 +88,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: "Poppins"),
       debugShowCheckedModeBanner: false,
-      home: NavBar(),
+      home: FeedbackPage(),
     );
   }
 }

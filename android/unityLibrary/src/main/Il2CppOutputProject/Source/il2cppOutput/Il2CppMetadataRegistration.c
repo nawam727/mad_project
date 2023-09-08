15285d626c9e3b9fd1eb0a02f544e3d201c18c1a
MokshaDill <mokshadil@gmail.com>
2023-09-08 07:49:13 +0530

    add src files in UL 03

---
+++ android/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,42 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+extern void** const g_MetadataUsages[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	4737,
+	g_Il2CppGenericTypes,
+	2455,
+	g_Il2CppGenericInstTable,
+	3111,
+	g_Il2CppGenericMethodFunctions,
+	16434,
+	g_Il2CppTypeTable,
+	11830,
+	g_Il2CppMethodSpecTable,
+	5514,
+	g_FieldOffsetTable,
+	5514,
+	g_Il2CppTypeDefinitionSizesTable,
+	53963,
+	g_MetadataUsages,
+};

42c4c4daf803139bae76aacef035aec6e2ac42f0
nawam727 <nawamdenawakage@gmail.com>
2023-09-13 15:36:32 +0530

    Create the hall availability backend

--- lib/main.dart
+++ lib/main.dart
@@ -63,34 +63,65 @@ class LectureData {
   );
 }
 
+class HallData {
+  final String b1101;
+  final String b1102;
+  final String b1110;
+  final String g002;
+  final String g009;
+  final String g003;
+
+  HallData(
+    this.b1101,
+    this.b1102,
+    this.b1110,
+    this.g002,
+    this.g009,
+    this.g003,
+  );
+}
+
+//Map Lecture Data
 Future<LectureData?> getLectureDetails() async {
   User? user = _auth.currentUser;
   print('User UID1: ${user?.uid}');
 
   if (user != null) {
-      DocumentSnapshot doc =
-          await _firestore.collection('lectures').doc(user.uid).get();
-      Map<String, dynamic>? lectureDataMap = doc.data() as Map<String, dynamic>?;
+    DocumentSnapshot doc =
+        await _firestore.collection('lectures').doc(user.uid).get();
+    Map<String, dynamic>? lectureDataMap = doc.data() as Map<String, dynamic>?;
 
-      if (lectureDataMap != null) {
-        LectureData lectureData = LectureData(
-          lectureDataMap.containsKey('lecture') ? lectureDataMap['lecture'] : '',
-          lectureDataMap.containsKey('lecture2') ? lectureDataMap['lecture2'] : '',
-          lectureDataMap.containsKey('lecturer') ? lectureDataMap['lecturer'] : '',
-          lectureDataMap.containsKey('lecturer2') ? lectureDataMap['lecturer2'] : '',
-          lectureDataMap.containsKey('location') ? lectureDataMap['location'] : '',
-          lectureDataMap.containsKey('location2') ? lectureDataMap['location2'] : '',
-          lectureDataMap.containsKey('time') ? lectureDataMap['time'] : '',
-          lectureDataMap.containsKey('time2') ? lectureDataMap['time2'] : '',
-        );
-        print('time: ${lectureData.lecture2}');
-        return lectureData;
-      }
+    if (lectureDataMap != null) {
+      LectureData lectureData = LectureData(
+        lectureDataMap.containsKey('lecture') ? lectureDataMap['lecture'] : '',
+        lectureDataMap.containsKey('lecture2')
+            ? lectureDataMap['lecture2']
+            : '',
+        lectureDataMap.containsKey('lecturer')
+            ? lectureDataMap['lecturer']
+            : '',
+        lectureDataMap.containsKey('lecturer2')
+            ? lectureDataMap['lecturer2']
+            : '',
+        lectureDataMap.containsKey('location')
+            ? lectureDataMap['location']
+            : '',
+        lectureDataMap.containsKey('location2')
+            ? lectureDataMap['location2']
+            : '',
+        lectureDataMap.containsKey('time') ? lectureDataMap['time'] : '',
+        lectureDataMap.containsKey('time2') ? lectureDataMap['time2'] : '',
+      );
+      // print('time: ${lectureData.lecture2}');
+      return lectureData;
+    }
   }
 
   return null;
 }
 
+
+//Map User Data
 Future<UserData?> getUserDetails() async {
   User? user = _auth.currentUser;
   print('User UID2: ${user?.uid}');
@@ -120,11 +151,39 @@ Future<UserData?> getUserDetails() async {
   return null;
 }
 
+//Map Hall Data
+Future<HallData?> getHallDetails() async {
+  User? user = _auth.currentUser;
+  print('User UID3: ${user?.uid}');
+
+  if (user != null) {
+    DocumentSnapshot doc =
+        await _firestore.collection('halls').doc(user.uid).get();
+    Map<String, dynamic>? hallDataMap = doc.data() as Map<String, dynamic>?;
+
+    if (hallDataMap != null) {
+      HallData hallData = HallData(
+        hallDataMap.containsKey('b1101') ? hallDataMap['b1101'] : '',
+        hallDataMap.containsKey('b1102') ? hallDataMap['b1102'] : '',
+        hallDataMap.containsKey('b1110') ? hallDataMap['b1110'] : '',
+        hallDataMap.containsKey('g002') ? hallDataMap['g002'] : '',
+        hallDataMap.containsKey('g009') ? hallDataMap['g009'] : '',
+        hallDataMap.containsKey('g003') ? hallDataMap['g003'] : '',
+      );
+      return hallData;
+    }
+  }
+
+  return null;
+}
+
 void fetchData() async {
   // Retrieve user data
   UserData? userData = await getUserDetails();
   // Retrieve lecture data
   LectureData? lectureData = await getLectureDetails();
+  // Retrieve lecture data
+  HallData? hallData = await getHallDetails();
 
   // Check if data is available and use it
   if (userData != null) {
@@ -140,10 +199,16 @@ void fetchData() async {
   } else {
     print('Lecture data not found.');
   }
+
+  if (hallData != null) {
+    print('Hall 1: ${hallData.b1101}');
+    print('Hall 2: ${hallData.g003}');
+  } else {
+    print('Hall data not found.');
+  }
 }
 
 Future<void> main() async {
-  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   fetchData();

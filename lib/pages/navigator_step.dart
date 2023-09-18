22ba81e142119e7ae836fa6f7e681f72871aa6b9
MokshaDill <mokshadil@gmail.com>
2023-09-18 13:51:11 +0530

    nav step page with modification

--- lib/pages/navigator_step.dart
+++ lib/pages/navigator_step.dart
@@ -4,6 +4,8 @@ import 'package:mad_project/components/back_dots.dart';
 import 'package:mad_project/components/back_menu.dart';
 import 'package:mad_project/components/back_notify.dart';
 
+import 'map_pages/map_page.dart';
+
 class NavigatorStepPage extends StatefulWidget {
   const NavigatorStepPage({super.key});
 
@@ -13,9 +15,19 @@ class NavigatorStepPage extends StatefulWidget {
 
 class _NavigatorStepPageState extends State<NavigatorStepPage> {
 
-  String? valueChoose;
-  String? valueChoose1;
-  List listItem = ["Main Gate", "Canteen", "Toilet", "Lecture hall"];
+  int? valueChoose;
+  int? valueChoose1;
+  int selectedIndex = 0;
+  int selectedIndex1 = 0;
+
+  //int? xp1 = int.tryParse(valueChoose);
+  //int? xp2 = int.tryParse(valueChoose1);
+
+  List listItem = ["Main Gate", "Canteen", "Com.Faculty", "Busi.Faculty"];
+  List listx = ['6.821344287173196','6.82124768770943','6.820235027249114','6.820639973882668'];
+  List listy = ['80.04157259862674','80.03782705843815','80.03953120216208','80.03901295034026'];
+
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -53,17 +65,17 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
                               child: DropdownButtonFormField(
                                 decoration:  InputDecoration(
                                   focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                      color: HexColor(
-                                          "#00B251")), // Border color when focused
-                                ),
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        color: HexColor(
+                                            "#00B251")), // Border color when focused
+                                  ),
                                   border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color:HexColor("#3F3F3F"),
-                                  )
-                                ),
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide(
+                                        color:HexColor("#3F3F3F"),
+                                      )
+                                  ),
                                   filled: true,
                                   fillColor: Color.fromARGB(255, 255, 255, 255),
                                 ),
@@ -87,7 +99,14 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
                                 value: valueChoose,
                                 onChanged: (newValue) {
                                   setState(() {
-                                    valueChoose = newValue as String?;
+                                    // Find the index of the selected item in the listItem list
+                                     selectedIndex = listItem.indexOf(newValue as String);
+
+                                    // Now, selectedIndex contains the index of the selected item.
+                                    // You can use it as needed.
+
+                                    // Update valueChoose1 with the index
+                                    valueChoose = selectedIndex;
                                   });
                                 },
                                 items: listItem.map((valueItem) {
@@ -133,11 +152,11 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
                                       color: HexColor(
                                           "#00B251")), // Border color when focused
                                 ),
-                               border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: BorderSide(
-                                    color:HexColor("#3F3F3F"),
-                                  )
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color:HexColor("#3F3F3F"),
+                                    )
                                 ),
                                 filled: true,
                                 fillColor: Color.fromARGB(255, 255, 255, 255),
@@ -159,7 +178,17 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
                               value: valueChoose1,
                               onChanged: (newValue1) {
                                 setState(() {
-                                  valueChoose1 = newValue1 as String?;
+                                  // Find the index of the selected item in the listItem list
+                                   selectedIndex1 = listItem.indexOf(newValue1 as String);
+
+                                  // Now, selectedIndex contains the index of the selected item.
+                                  // You can use it as needed.
+
+                                  // Update valueChoose1 with the index
+                                  valueChoose1 = selectedIndex1;
+                                   print('------------------');
+
+                                   print(selectedIndex1);
                                 });
                               },
                               items: listItem.map((valueItem) {
@@ -192,15 +221,13 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
           //background image
           Expanded(
             child: Container(
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/images/Map.png"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
                 child: Column(
                   children: [
                     Expanded(
+                        child:MapScreen(x1: listx[selectedIndex], y1: listy[selectedIndex],x2: listx[2], y2: listy[2])
+                    ),
+                    //bottom container that has popup function
+                    Container(
                       child: Container(),
                     ),
                     //bottom container that has popup function
@@ -247,40 +274,41 @@ class _NavigatorStepPageState extends State<NavigatorStepPage> {
 
 //popup container
   Widget buildSheet() => makeDismissible(
-    
-        child: DraggableScrollableSheet(
-          initialChildSize: 0.3,
-          minChildSize: 0.2,
-          maxChildSize: 0.5,
-          builder: (_, controller) => Container(
-            decoration: BoxDecoration(
-              border: Border.all(width: 1,color:HexColor("#3F3F3F")),
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(16),
-              child: ListView(
-                controller: controller,
-                children:  [
-                  IconButton(
-                          onPressed: () {
-                             Navigator.of(context).pop();
-                          },
-                          icon: const Icon(
-                            Icons.expand_more_sharp,
-                            color:Colors.black54
-                          ),
-                          iconSize: 50,
-                        ),
-                  Text(
-                      "Dolore esse duis Lorem amet id do aute dolor eiusmod tempor aliquip exercitation. Eiusmod esse exercitation nulla fugiat labore eiusmod ad laborum velit ut pariatur laboris occaecat esse. Deserunt nulla elit reprehenderit exercitation dolore ea ex mollit. Deserunt consequat minim Lorem tempor officia proident sunt culpa sunt fugiat aute ex nostrud. Laborum duis dolor voluptate ea deserunt amet velit proident labore deserunt mollit cillum. Proident tempor ipsum officia non officia ipsum anim."),
-                  Text(
-                      "Officia laboris aute irure ipsum dolore Lorem proident. Officia ex elit ad consequat fugiat sunt mollit dolor eu adipisicing. Adipisicing qui proident tempor id duis do qui minim minim ullamco adipisicing ipsum. Adipisicing sit fugiat irure laborum do. Laboris Lorem tempor et deserunt aute esse ad cillum. Pariatur sunt sunt commodo elit.")
-                ],
+
+    child: DraggableScrollableSheet(
+      initialChildSize: 0.3,
+      minChildSize: 0.2,
+      maxChildSize: 0.5,
+      builder: (_, controller) => Container(
+        decoration: BoxDecoration(
+          border: Border.all(width: 1,color:HexColor("#3F3F3F")),
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView(
+            controller: controller,
+            children:  [
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                    Icons.expand_more_sharp,
+                    color:Colors.black54
+                ),
+                iconSize: 50,
               ),
-            ),
+              Text(
+                  "Dolore esse duis Lorem amet id do aute dolor eiusmod tempor aliquip exercitation. Eiusmod esse exercitation nulla fugiat labore eiusmod ad laborum velit ut pariatur laboris occaecat esse. Deserunt nulla elit reprehenderit exercitation dolore ea ex mollit. Deserunt consequat minim Lorem tempor officia proident sunt culpa sunt fugiat aute ex nostrud. Laborum duis dolor voluptate ea deserunt amet velit proident labore deserunt mollit cillum. Proident tempor ipsum officia non officia ipsum anim."),
+              Text(
+                  "Officia laboris aute irure ipsum dolore Lorem proident. Officia ex elit ad consequat fugiat sunt mollit dolor eu adipisicing. Adipisicing qui proident tempor id duis do qui minim minim ullamco adipisicing ipsum. Adipisicing sit fugiat irure laborum do. Laboris Lorem tempor et deserunt aute esse ad cillum. Pariatur sunt sunt commodo elit.")
+            ],
           ),
         ),
-      );
+      ),
+    ),
+  );
 }
+
